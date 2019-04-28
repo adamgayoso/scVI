8f765339b4ecb86a8bfa6dc271ae07e4ff00a479
adamgayoso <adamgayoso@users.noreply.github.com>
2019-04-28 13:59:49 -0700

    use numpy for sqrt of scalar

--- scvi/models/vae_atac.py
+++ scvi/models/vae_atac.py
@@ -257,7 +257,7 @@ class VAE_ATAC(nn.Module):
             scale = torch.ones_like(qz_v)
         else:
             mean = (np.log(ap) - (1 / self.n_latent)*(self.n_latent*np.log(ap))) * torch.ones_like(qz_m)
-            scale = (torch.sqrt((1 / ap)*(1 - 2 / self.n_latent) + (1 / self.n_latent**2)*(self.n_latent*1/ap))) * torch.ones_like(qz_v)
+            scale = (np.sqrt((1 / ap)*(1 - 2 / self.n_latent) + (1 / self.n_latent**2)*(self.n_latent*1/ap))) * torch.ones_like(qz_v)
 
         kl_divergence_z = kl(Normal(qz_m, torch.sqrt(qz_v)), Normal(mean, scale)).sum(dim=1)
         if self.reconstruction_loss not in ['beta-bernoulli', 'zero_inflated_bernoulli', 'bernoulli', 'multinomial', 'lda']:
